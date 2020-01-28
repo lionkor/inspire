6bb65121ff225f5b59665994350f5309dd8ebcca
Lion Kortlepel <development@kortlepel.com>
2020-01-28 16:51:32 +0100

    Complete feature "give", remove "Result" enum
    we use a custom vector class to handle random lines.

--- main.c
+++ main.c
@@ -1,20 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 #include <assert.h>
 #include <sys/stat.h>
 #include <errno.h>
-
-typedef enum Result
-{
-    OK           = 0x000, // everything is okay
-    NO_ARGS      = 0x001, // no arguments given
-    UNKNOWN_ARGS = 0x002, // given argument(s) not known
-    BAD_ARGC     = 0x003, // argument count invalid
-    MKDIR_ERROR  = 0x004, // mkdir failed
-    FOPEN_ERROR  = 0x005, // fopen failed
-    FCLOSE_ERROR = 0x006, // fclose failed (yes that's a thing)
-} Result;
 
 #define ENDL "\n"
 #define STR_BAD_ARGC_ADD "expected one or more arguments to `add`. run `inspire help` to show help." ENDL
@@ -31,16 +21,53 @@ typedef enum Result
 #define STR_HELP_SHORT \
     "Unknown usage. Run 'inspire help' to show help.\n"
 
+typedef struct vector {
+    void*  data;
+    size_t size;
+    size_t capacity;
+    size_t elem_size;
+} vector_t;
 
-Result
-command_add(int argc, char** argv) {
+vector_t* vector_create(size_t elem_size, size_t reserve) {
+    vector_t* v  = (vector_t*)malloc(sizeof(vector_t));
+    v->data      = calloc(reserve, elem_size);
+    v->capacity  = reserve;
+    v->size      = 0;
+    v->elem_size = elem_size;
+    if (reserve > 0)
+        memset(v->data, 0, reserve);
+    return v;
+}
+
+void vector_free(vector_t* v) {
+    if (v)
+        free(v->data);
+    free(v);
+}
+
+void vector_push_back(vector_t* v, void* elem) {
+    assert(v); // we just want to crash, since this is not a public api
+    assert(elem);
+    if (v->size + 1 > v->capacity) {
+        v->data = realloc(v->data, v->elem_size * (v->size + 1));
+        v->capacity++;
+    }
+    memcpy(v->data + v->size * v->elem_size, elem, v->elem_size);
+    v->size++;
+}
+
+void* vector_at(vector_t* v, size_t index) {
+    return v->data + index * v->elem_size;
+}
+
+int command_add(int argc, char** argv) {
     // argv[0] -> program name
     // argv[1] -> command "add"
     // argv[2] => idea string(s) to add
     if (argc == 2) {
         // not enough args
         fprintf(stderr, STR_BAD_ARGC_ADD);
-        return BAD_ARGC;
+        return -1;
     }
 
     char* home_dir = getenv("HOME");
@@ -53,7 +80,7 @@ command_add(int argc, char** argv) {
     if (rc != 0 && errno != EEXIST) {
         fprintf(stderr, STR_UNKNOWN_ERROR);
         perror("mkdir");
-        return MKDIR_ERROR;
+        return -1;
     }
 
     strcat(dir, "/data");
@@ -64,7 +91,7 @@ command_add(int argc, char** argv) {
     if (!fp) {
         fprintf(stderr, STR_UNKNOWN_ERROR);
         perror("fopen");
-        return FOPEN_ERROR;
+        return -1;
     }
 
     /* 
@@ -82,23 +109,69 @@ command_add(int argc, char** argv) {
     if (rc != 0) {
         fprintf(stderr, STR_UNKNOWN_ERROR);
         perror("fclose");
-        return FCLOSE_ERROR;
+        return -1;
     }
 
-    return OK;
+    return 0;
 }
 
-Result command_give() {
+int command_give() {
+    char* home_dir = getenv("HOME");
+    char  dir[1024];
+    memset(dir, 0, 1024);
+    strcpy(dir, home_dir);
+    strcat(dir, "/.inspire");
 
-    return OK;
+    int rc = mkdir(dir, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    if (rc != 0 && errno != EEXIST) {
+        fprintf(stderr, STR_UNKNOWN_ERROR);
+        perror("mkdir");
+        return -1;
+    }
+
+    strcat(dir, "/data");
+
+    FILE* fp = fopen(dir, "r");
+    if (!fp) {
+        fprintf(stderr, STR_UNKNOWN_ERROR);
+        perror("fopen");
+        return -1;
+    }
+
+    // 1 KB max line length
+    size_t max_len = 1024;
+    char   line[max_len];
+    // reserve space for 10 lines
+    // after 10 lines reallocs will happen with vector_push_back
+    vector_t* lines = vector_create(sizeof(char) * max_len, 10);
+    while (fgets(line, max_len, fp) != NULL) {
+        // copy max_len chars from line into the vector
+        vector_push_back(lines, line);
+    }
+
+    if (lines->size == 0) {
+        printf("no ideas stored!\n");
+    } else {
+        // no need for newline since the lines end with \n anyways
+        printf("%s", vector_at(lines, rand() % lines->size));
+    }
+
+    vector_free(lines);
+
+    return 0;
 }
 
-Result command_help() {
+int command_help() {
     printf("%s", STR_HELP);
-    return OK;
+    return 0;
 }
 
 int main(int argc, char** argv) {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    // using nanoseconds to seed in case we call give more than once a second (likely)
+    srand((time_t)ts.tv_nsec);
+
     if (argc > 1) {
         if (strcmp(argv[1], "add") == 0) {
             return command_add(argc, argv);
@@ -110,6 +183,6 @@ int main(int argc, char** argv) {
     }
 
     printf("%s", STR_HELP_SHORT);
-    return UNKNOWN_ARGS;
+    return -1;
 }
 
